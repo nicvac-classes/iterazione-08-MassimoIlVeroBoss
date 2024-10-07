2497926519a53776c1dbbb21d2939c97aea5e7f6
MassimoIlVeroBoss <claudio.mercoledisanto@marconibari.edu.it>
2024-10-07 16:26:04 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+  int N, M, Media, MediaClasse, Debito, NoDebito;
+  string Nome;
+  M = 0;
+  Debito = 0;
+  NoDebito = 0;
+  MediaClasse = 0;
+  do {
+    cout << "Quanti studenti ci sono in classe?";
+    cin >> N;
+  }
+  while (N <= 0);
+  while (M < N){
+    cout << "Inserire nome: ";
+    cin >> Nome;
+    cout << "Inserire media di informatica";
+    MediaClasse = MediaClasse + Media;
+    if (Media < 6){
+        cout << "Prenderai il debito";
+        Debito = Debito + 1;
+    }else{
+        cout << "Non prenderai il debito";
+        NoDebito =  NoDebito + 1;
+    }
+       M = M + 1;
+  }
+     MediaClasse = MediaClasse / N;
+     cout << "Gli studenti che prenderanno il debito sono " << Debito << ", quelli che non prenderanno il debito sono " << NoDebito << ". La media della classe è " << MediaClasse; 
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
